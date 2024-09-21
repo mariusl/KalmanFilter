66904afc9d229ebe720ad786a7d7cddbb5fd46aa
mariusl <marius@bluearccnc.com>
2024-09-21 14:51:45 +0200

    Fixed license stuff

--- examples/basic.ino
+++ examples/basic.ino
@@ -1,3 +1,19 @@
+/*
+ * Example using Kalman filter
+ *
+ * Copyright (C) 2024  Marius Liebenberg <marius@protomac.co.za>
+ *
+ * THIS MATERIAL IS PROVIDED AS IS, WITH ABSOLUTELY NO WARRANTY EXPRESSED
+ * OR IMPLIED. ANY USE IS AT YOUR OWN RISK.
+ *
+ * Permission is hereby granted to use or copy this program
+ * for any purpose, provided the above notices are retained on all copies.
+ * Permission to modify the code and to distribute modified code is granted,
+ * provided the above notices are retained, and a notice that the code was
+ * modified is included with the above copyright notice.
+ *
+ */
+
 #include <KalmanFilter.h>
 
 // Create a Kalman filter object
